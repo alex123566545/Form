3f732b60e9935c74d73772624ab82aa4d85cb863
alexAlcocerR <axelxyz1234@gmail.com>
2023-07-18 17:50:09 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'formulario2';
+export class AppComponent implements OnInit{
+  form:FormGroup;
+constructor(private fb: FormBuilder){
+  this.form = this.fb.group({
+    name:['',Validators.required],
+    description:['',Validators.required],
+    tickness:['',Validators.required],
+    width:['',Validators.required],
+    height:['',Validators.required],
+    finished:['',Validators.required],
+    suplier:['',Validators.required,],
+    category:['',Validators.required],
+    level:['',Validators.required]
+
+  })
+}
+
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
